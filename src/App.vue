a23aaad344842f545fe6e0f88637f77f7728f64c
Patrick Kim <patrick.yrkim@gmail.com>
2021-11-27 00:28:44 -0800

    Add modal component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <h1>{{ title }}</h1>
+  <!-- <input type="text" ref="name"> -->
+  <!-- <button @click="handleClick">click me</button> -->
+  <Modal />
 </template>
 
 <script>
+// import components
+import Modal from "./components/Modal"
+
 // root component
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'my first vue app'
     }
+  },
+  methods: {
+    // handleClick() {
+    //   // use refs to get dom elements
+    //   // console.log(this.$refs.name)
+    //   // this.$refs.name.classList.add('active')
+    //   this.$refs.name.focus()
+    // }
   }
 }
 </script>
@@ -22,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal Title</h1>
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<script>
+// export default {
+//     name: 'Modal'
+// }
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
